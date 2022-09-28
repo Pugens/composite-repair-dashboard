0e486080e06aadb326820cd1051905b95c5f570b
Eugenio Bernardi <e.bernardi@tudelft.nl>
2022-09-28 12:09:12 +0200

    cleaning, fixed some layout

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import io
 import time
 import os
 import base64
-from turtle import onrelease
 
 from dash import Dash, dcc, html, dash_table, Input, Output, State
 import plotly.express as px
@@ -43,14 +42,13 @@ def pil_to_b64_dash(im):
     return bytes("data:image/jpeg;base64,", encoding="utf-8") + img_str
 
 
-def parse_coa_contents(contents, filename, date):
+def parse_pdf_contents(contents, filename, date):
     content_type, content_string = contents.split(",")
 
     decoded = base64.b64decode(content_string)
-
     images = convert_from_bytes(
         decoded,
-        poppler_path=r"C:\Users\eugeniobernard\Local_documents\dash\poppler-0.68.0\bin",
+        poppler_path=poppler_path,
     )
 
     encoded = pil_to_b64_dash(images[0])
@@ -67,8 +65,11 @@ def parse_coa_contents(contents, filename, date):
 
 # endregion
 
-# region Main App
+# region settings
 app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+# path to pdf decoding library:
+poppler_path = os.path.join(os.getcwd(), "lib\\poppler-0.68.0\\bin")
 
 # path to .csv files:
 oems_table_path = os.path.join(os.getcwd(), "tables", "oems_table.csv")
@@ -83,6 +84,30 @@ prod_list = prod_table["Product"]
 repairs_table = pd.read_csv(repairs_table_path)
 repairs_list = repairs_table["Repair"]
 
+# file uploads style:
+image_uplaod_style = {
+    "width": "100%",
+    "height": "60px",
+    "lineHeight": "60px",
+    "borderWidth": "1px",
+    "borderStyle": "dashed",
+    "borderRadius": "5px",
+    "textAlign": "center",
+    "margin": "10px",
+}
+pdf_uplaod_style = {
+    "width": "100%",
+    "height": "60px",
+    "lineHeight": "60px",
+    "borderWidth": "1px",
+    "borderStyle": "dashed",
+    "borderRadius": "5px",
+    "textAlign": "center",
+    "margin": "10px",
+}
+# endregion
+
+# region app layout
 app.layout = html.Div(
     children=[
         html.Div(
@@ -124,84 +149,86 @@ app.layout = html.Div(
                 html.Div(
                     [
                         "Picture name: ",
-                        dcc.Input(id="my-input", value="", type="text"),
+                        dcc.Input(id="picture-input-name", value="", type="text"),
                     ]
                 ),
                 spacing(),
-                html.Div(id="my-output"),
+                html.Div(id="picture-output-name"),
                 spacing(),
-                dcc.Upload(
-                    id="upload-image",
-                    children=html.Div(["Drag and Drop or ", html.A("Select picture")]),
-                    style={
-                        "width": "100%",
-                        "height": "60px",
-                        "lineHeight": "60px",
-                        "borderWidth": "1px",
-                        "borderStyle": "dashed",
-                        "borderRadius": "5px",
-                        "textAlign": "center",
-                        "margin": "10px",
-                    },
-                    # Allow multiple files to be uploaded
-                    multiple=True,
-                ),
-                spacing(),
-                dcc.Upload(
-                    className="four columns",
-                    id="upload-coa",
-                    children=html.Div(["Drag and Drop or ", html.A("Select PDF")]),
-                    style={
-                        "width": "45%",
-                        "height": "60px",
-                        "lineHeight": "60px",
-                        "borderWidth": "1px",
-                        "borderStyle": "dashed",
-                        "borderRadius": "5px",
-                        "textAlign": "center",
-                        "margin": "10px",
-                    },
-                    # Allow multiple files to be uploaded
-                    multiple=False,
+                html.Div(
+                    [
+                        dcc.Upload(
+                            className="three columns",
+                            id="upload-image",
+                            children=html.Div(
+                                ["Drag and Drop or ", html.A("Select picture")]
+                            ),
+                            style=image_uplaod_style,
+                            # Allow multiple files to be uploaded
+                            multiple=True,
+                        ),
+                        dcc.Upload(
+                            className="three columns",
+                            id="upload-coa",
+                            children=html.Div(
+                                ["Drag and Drop or ", html.A("Select PDF")]
+                            ),
+                            style=pdf_uplaod_style,
+                            # Allow multiple files to be uploaded
+                            multiple=False,
+                        ),
+                        dcc.Upload(
+                            className="three columns",
+                            id="upload-pcd",
+                            children=html.Div(
+                                [
+                                    "Drag and Drop or ",
+                                    html.A("Select a pointcloud file"),
+                                ]
+                            ),
+                            style=pdf_uplaod_style,
+                            # Allow multiple files to be uploaded
+                            multiple=False,
+                            accept=".ply, .pcd",
+                        ),
+                    ],
+                    style={"display": "flex"},
+                    className="row",
                 ),
             ],
         ),
         html.Hr(),
-        html.Div(id="output-coa"),
-        html.Hr(),
-        html.Div(id="output-image"),
+        html.Div(
+            [
+                html.Div(id="output-coa", className="three columns"),
+                html.Div(id="output-image-upload", className="three columns"),
+                html.Div(id="pcd-render", className="three columns"),
+            ],
+            style={"display": "flex"},
+            className="row",
+        ),
     ]
 )
 
 # endregion
 
 # region Callbacks
-# Show uploaded image
 @app.callback(
-    Output("output-image", "children"),
-    [Input("upload-image", "contents")],
-    [State("upload-image", "filename"), State("upload-image", "last_modified")],
-)
-def update_output(list_of_contents, list_of_names, list_of_dates):
-    if list_of_contents is not None:
-        children = [
-            parse_contents(c, n, d)
-            for c, n, d in zip(list_of_contents, list_of_names, list_of_dates)
-        ]
-        return children
-
-
-@app.callback(Output("output", "children"), [Input("OEM-dropdown", "value")])
-def display_output(value):
-    return str(value)
-
-
-@app.callback(
-    Output(component_id="my-output", component_property="children"),
-    Input(component_id="my-input", component_property="value"),
+    Output(component_id="picture-output-name", component_property="children"),
+    Input(component_id="picture-input-name", component_property="value"),
 )
 def update_output_div(input_value):
     return f"Output: {input_value}"
+
+
+# @app.callback(
+#     Output(component_id="upload-pcd", component_property="children"),
+#     Input(component_id="pcd-render", component_property="contents"),
+# )
+# def load_pcd_file(list_of_contents):
+#     if list_of_contents is not None:
+#         children = [parse_contents(c, n, d) for c, n, d in zip(list_of_contents)]
+#         return children
 
 
 @app.callback(
@@ -226,7 +253,7 @@ def update_output(list_of_contents, list_of_names, list_of_dates):
 )
 def show_coa(list_of_contents, list_of_names, list_of_dates):
     if list_of_contents is not None:
-        children = [parse_coa_contents(list_of_contents, list_of_names, list_of_dates)]
+        children = [parse_pdf_contents(list_of_contents, list_of_names, list_of_dates)]
         return children
 
 
